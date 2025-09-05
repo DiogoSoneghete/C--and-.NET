0bb23ef013423354e1e1ec2ed901c29900df821f
DiogoSoneghete <diogosonegueti@gmail.com>
2025-09-05 14:48:09 -0300

    Comparisons basics

--- Program.cs
+++ Program.cs
@@ -7,27 +7,18 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("\n\n-----Verificar Impar ou Par-----\n\n");
 
-        Console.WriteLine("\n\n-- Calculadora Simples --\n\n");
-        Console.WriteLine("Digite o primeiro número: ");
-        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digite um número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Digite o segundo número: ");
-        int n2 = Convert.ToInt32(Console.ReadLine());
-
-        int som = n1 + n2;
-        int sub = n1 - n2;
-        int mult = n1 * n2;
-        int div = n1 / n2;
-        int module = n1 % n2;
-
-        Console.WriteLine("O resultado da soma é: " + som);
-        Console.WriteLine("O resultado da subtração é: " + sub);
-        Console.WriteLine("O resultado da multiplicação é: " + mult);
-        Console.WriteLine("O resultado da divisão é: " + div);
-        Console.WriteLine("O resultado do resto da divisão é: " + module + "\n\n");
-    
-    
-        
+        if(numero % 2 == 0)
+        {
+            Console.WriteLine($"O número {numero} é par\n\n");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numero} é ímpar\n\n");
+        }
     }
 }
